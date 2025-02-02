c3e34388a8f3660d27fb8412f72d8b9251086f9c
Kehvarl <Kehvarl@Kehvarl.com>
2025-02-01 22:59:25 -0700

    added knights

--- app/main.rb
+++ app/main.rb
@@ -1,85 +1,62 @@
+def init args
+  args.state.dragon = {v:5, x:620, y:0, w:40, h:80, path:'sprites/square/black.png'}.sprite!
+  args.state.knights = []
+  args.state.running = false
+end
+
+def spawn_knight args
+  k = {v:2, x:0, y:680, w:90, h:40}
+  if not args.state.knights.any_intersect_rect?(k)
+    args.state.knights << {v:2, x:0, y:680, w:60, h:40, flip_horizontally: false, path:'sprites/square/red.png'}.sprite!
+  end
+end
+
 def tick args
-  args.state.logo_rect ||= { x: 576,
-                             y: 200,
-                             w: 128,
-                             h: 101 }
+  if args.tick_count == 0
+    init args
+  end
 
-  args.outputs.labels  << { x: 640,
-                            y: 600,
-                            text: 'Hello World!',
-                            size_px: 30,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 510,
-                            text: "Documentation is located under the ./docs/docs.txt directory. 150+ samples are located under the ./samples directory.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 480,
-                            text: "If you prefer formatted docs, you can access them locally at http://localhost:9001 or online at http://docs.dragonruby.org.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 400,
-                            text: "The code that powers what you're seeing right now is located at ./mygame/app/main.rb.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 380,
-                            text: "(you can change the code while the app is running and see the updates live)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.sprites << { x: args.state.logo_rect.x,
-                            y: args.state.logo_rect.y,
-                            w: args.state.logo_rect.w,
-                            h: args.state.logo_rect.h,
-                            path: 'dragonruby.png',
-                            angle: Kernel.tick_count }
-
-  args.outputs.labels  << { x: 640,
-                            y: 180,
-                            text: "(use arrow keys to move the logo around)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 80,
-                            text: 'Join the Discord Server! https://discord.dragonruby.org',
-                            size_px: 30,
-                            anchor_x: 0.5 }
+  if args.state.knights.size < 10
+    spawn_knight args
+  end
 
   if args.inputs.keyboard.left
-    args.state.logo_rect.x -= 10
+    # Fly Left
+    args.state.dragon.x = [0, args.state.dragon.x - args.state.dragon.v].max
   elsif args.inputs.keyboard.right
-    args.state.logo_rect.x += 10
+    # Fly Right
+    args.state.dragon.x = [1240, args.state.dragon.x + args.state.dragon.v].min
   end
 
-  if args.inputs.keyboard.down
-    args.state.logo_rect.y -= 10
-  elsif args.inputs.keyboard.up
-    args.state.logo_rect.y += 10
+  if args.inputs.keyboard.up
+    # Move away from the cave
+    args.state.dragon.y = [120, args.state.dragon.y + args.state.dragon.v].min
+  elsif args.inputs.keyboard.down
+    # Move closer to the cave
+    args.state.dragon.y = [0, args.state.dragon.y - args.state.dragon.v].max
   end
 
-  if args.state.logo_rect.x > 1280
-    args.state.logo_rect.x = 0
-  elsif args.state.logo_rect.x < 0
-    args.state.logo_rect.x = 1280
+  if args.inputs.keyboard.space
+    #Breathe fire
+    args.state.running = true
   end
 
-  if args.state.logo_rect.y > 720
-    args.state.logo_rect.y = 0
-  elsif args.state.logo_rect.y < 0
-    args.state.logo_rect.y = 720
+  if not args.state.running
+    return
   end
+
+  knights = args.state.knights.map do |k|
+    k.x += k.v
+    if k.x + k.w >= 1280 or k.x <= 0
+      k.v = -k.v
+      k.y -= (k.h + 10)
+      k.flip_horizontally = !k.flip_horizontally
+    end
+
+  end
+
+  # Render
+  args.outputs.primitives << {x:0, y:0, w:1280, h:720, r:0, g:96, b:32}.solid!
+  args.outputs.primitives << args.state.dragon
+  args.outputs.primitives << args.state.knights
 end
